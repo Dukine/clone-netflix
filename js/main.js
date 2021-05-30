a0f4bc883207edb6d1e0ad29ebb7f44b9d231a5f
Dukine <gabrielwrnck@gmail.com>
2021-05-30 08:21:28 -0300

    Os botões direcionam para um trailer e uma página de informações

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+function redirecionar(elemento){
+
+    switch (elemento){
+        case 1: //Trailer
+            window.open("https://youtu.be/kasvV-XyzqU");
+        break;
+
+        case 2: //Mais informações
+            window.open("https://g.co/kgs/6z4Q9j");
+            break;
+    }
+
+
+
+}
