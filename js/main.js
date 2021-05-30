09e6dc28e86081c9652a077447c0cb2909b2b047
Dukine <gabrielwrnck@gmail.com>
2021-05-30 08:54:50 -0300

    Página personalizada para se tornar voltada a games

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ function redirecionar(elemento){
 
     switch (elemento){
         case 1: //Trailer
-            window.open("https://youtu.be/kasvV-XyzqU");
+            window.open("https://youtu.be/d9Gu1PspA3Y");
         break;
 
         case 2: //Mais informações
-            window.open("https://g.co/kgs/6z4Q9j");
+            window.open("https://store.steampowered.com/app/397540/Borderlands_3/");
             break;
     }
 
